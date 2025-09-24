eaee0d1173fb304a04e05156f928683b1397a9c2
AliaJimenez <20251018@itla.edu.do>
2025-09-24 13:58:12 -0400

    Add files via upload

---
+++ datospersonales/src/datospersonales/Datospersonales.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package datospersonales;
+
+/**
+ *
+ * @author alian
+ */
+public class Datospersonales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Realizar un programa que imprima por pantalla su nombre y correo electrónico.
+        System.out.println("Aliandy Jimenez");
+        System.out.println("20251018@itla.edu.do");
+    }
+    
+}
